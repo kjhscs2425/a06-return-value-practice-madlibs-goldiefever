aa19eebd1191e9ea763afef4633b62f1f959f7db
drebee <ebennett@kehillah.org>
2024-11-19 09:59:51 -0800

    I added some spaces to make it print nicer

--- main.py
+++ main.py
@@ -28,19 +28,19 @@ place1= input("Give me a place: ")
 
 story = (f"A vacation is when you take a trip to some {adj1} place \
 with your {adj2} family. Usually you go to some place \
-that is near a/an {noun1} or up on a/an {noun2}.\
-A good vacation place is one where you can ride {pluralnoun1}\
-or play {game1} or go hunting for {pluralnoun2} . I like\
-to spend my time {verb_ing1} or {verb_ing2}.\
-When parents go on a vacation, they spend their time eating\
-three {pluralnoun3} a day, and fathers play golf, and mothers\
-sit around {verb_ing3}. Last summer, my little brother\
-fell in a/an {noun3} and got poison {plant1} all\
-over his {bodypart1}. My family is going to go to (the) {place1}\
-, and I will practice {verb_ing4}. Parents\
-need vacations more than kids because parents are always very\
-{adj3} and because they have to work {number1}\
-hours every day all year making enough {pluralnoun4} to pay\
+that is near a/an {noun1} or up on a/an {noun2}. \
+A good vacation place is one where you can ride {pluralnoun1} \
+or play {game1} or go hunting for {pluralnoun2} . I like \
+to spend my time {verb_ing1} or {verb_ing2}. \
+When parents go on a vacation, they spend their time eating \
+three {pluralnoun3} a day, and fathers play golf, and mothers \
+sit around {verb_ing3}. Last summer, my little brother \
+fell in a/an {noun3} and got poison {plant1} all \
+over his {bodypart1}. My family is going to go to (the) {place1} \
+, and I will practice {verb_ing4}. Parents \
+need vacations more than kids because parents are always very \
+{adj3} and because they have to work {number1} \
+hours every day all year making enough {pluralnoun4} to pay \
 for the vacation.")
 
 print(story)
